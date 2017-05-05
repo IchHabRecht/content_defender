8fed2c637af2aee0086c8c0c6f8bd4db73463668
Nicole Cordes <typo3@cordes.co>
2017-05-05 23:20:13 +0200

    [FEATURE] Resolve allowed CType items in TcaCTypeItems data provider

--- Form/FormDataProvider/TcaCTypeItems.php
+++ Form/FormDataProvider/TcaCTypeItems.php
@@ -1,7 +1,9 @@
 <?php
 namespace IchHabRecht\ContentDefender\Form\FormDataProvider;
 
+use IchHabRecht\ContentDefender\BackendLayout\BackendLayoutConfiguration;
 use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class TcaCTypeItems implements FormDataProviderInterface
 {
@@ -11,5 +13,40 @@ class TcaCTypeItems implements FormDataProviderInterface
      */
     public function addData(array $result)
     {
+        if ('tt_content' !== $result['tableName']) {
+            return $result;
+        }
+
+        $pageId = !empty($result['effectivePid']) ? (int)$result['effectivePid'] : (int)$result['databaseRow']['pid'];
+        $backendLayoutConfiguration = BackendLayoutConfiguration::createFromPageId($pageId);
+        if (null === $backendLayoutConfiguration) {
+            return $result;
+        }
+
+        $colPos = (int)$result['databaseRow']['colPos'];
+        $columnConfiguration = $backendLayoutConfiguration->getConfigurationByColPos($colPos);
+        if (empty($columnConfiguration) || (empty($columnConfiguration['allowed']) && empty($columnConfiguration['disallowed']))) {
+            return $result;
+        }
+
+        if (!empty($columnConfiguration['allowed'])) {
+            $cTypes = GeneralUtility::trimExplode(',', $columnConfiguration['allowed']);
+            $result['processedTca']['columns']['CType']['config']['items'] = array_filter(
+                $result['processedTca']['columns']['CType']['config']['items'],
+                function ($item) use ($cTypes) {
+                    return in_array($item[1], $cTypes);
+                }
+            );
+        } else {
+            $cTypes = GeneralUtility::trimExplode(',', $columnConfiguration['disallowed']);
+            $result['processedTca']['columns']['CType']['config']['items'] = array_filter(
+                $result['processedTca']['columns']['CType']['config']['items'],
+                function ($item) use ($cTypes) {
+                    return !in_array($item[1], $cTypes);
+                }
+            );
+        }
+
+        return $result;
     }
 }
