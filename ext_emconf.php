1695bf66731db4dd195e2c02b8576d908782a96f
Nicole Cordes <typo3@cordes.co>
2017-05-05 18:03:08 +0200

    [TASK] Add basic extension files

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "content_defender".
+ *
+ * Auto generated 05-05-2017 18:02
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Content Defender',
+  'description' => 'Define allowed or denied content element types in your backend layouts',
+  'category' => 'plugin',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'CPS-IT GmbH',
+  'state' => 'alpha',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:1:{s:12:"ext_icon.png";s:4:"f488";}',
+);
+
