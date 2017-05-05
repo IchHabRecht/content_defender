d13683033362e52a80458c4b77756562eaccb8f5
Nicole Cordes <typo3@cordes.co>
2017-05-05 18:18:49 +0200

    [TASK] Add empty data provider

---
+++ Classes/Form/FormDataProvider/TcaCTypeItems.php
@@ -0,0 +1,15 @@
+<?php
+namespace IchHabRecht\ContentDefender\Form\FormDataProvider;
+
+use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+
+class TcaCTypeItems implements FormDataProviderInterface
+{
+    /**
+     * @param array $result
+     * @return array
+     */
+    public function addData(array $result)
+    {
+    }
+}
