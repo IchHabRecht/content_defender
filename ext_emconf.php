fdc53cd01bfb9d36549832a468d3245c7d292ef5
Nicole Cordes <typo3@cordes.co>
2017-05-10 12:28:39 +0200

    [RELEASE] Release of content_defender 1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "content_defender".
  *
- * Auto generated 08-05-2017 11:39
+ * Auto generated 10-05-2017 12:28
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -17,11 +17,11 @@ $EM_CONF[$_EXTKEY] = array (
   'author' => 'Nicole Cordes',
   'author_email' => 'typo3@cordes.co',
   'author_company' => 'CPS-IT GmbH',
-  'state' => 'alpha',
+  'state' => 'beta',
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.0.1',
+  'version' => '1.0.2',
   'constraints' => 
   array (
     'depends' => 
@@ -35,6 +35,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"7b5a";s:9:"README.md";s:4:"b63d";s:13:"composer.json";s:4:"6028";s:12:"ext_icon.png";s:4:"f488";s:17:"ext_localconf.php";s:4:"974f";s:52:"Classes/BackendLayout/BackendLayoutConfiguration.php";s:4:"6cbb";s:47:"Classes/Form/FormDataProvider/TcaCTypeItems.php";s:4:"2495";s:38:"Classes/Hooks/CTypeDataHandlerHook.php";s:4:"d3e4";s:33:"Classes/Hooks/WizardItemsHook.php";s:4:"27da";}',
+  '_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"7cab";s:9:"README.md";s:4:"b63d";s:13:"composer.json";s:4:"ce5e";s:12:"ext_icon.png";s:4:"f488";s:17:"ext_localconf.php";s:4:"974f";s:52:"Classes/BackendLayout/BackendLayoutConfiguration.php";s:4:"1042";s:47:"Classes/Form/FormDataProvider/TcaCTypeItems.php";s:4:"2495";s:38:"Classes/Hooks/CTypeDataHandlerHook.php";s:4:"c4b1";s:33:"Classes/Hooks/WizardItemsHook.php";s:4:"27da";s:44:"Tests/Functional/Fixtures/backend_layout.xml";s:4:"93a4";s:35:"Tests/Functional/Fixtures/pages.xml";s:4:"85f0";s:40:"Tests/Functional/Fixtures/tt_content.xml";s:4:"57e9";s:51:"Tests/Functional/Hooks/CTypeDataHandlerHookTest.php";s:4:"38a1";}',
 );
 
