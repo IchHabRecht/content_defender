29ceb6d1e0a101dbf74d2ec9ba21d5eedc487861
Nicole Cordes <typo3@cordes.co>
2017-05-10 12:05:17 +0200

    [BUGFIX] Only restrict move and copy commands from DataHandler

--- Classes/Hooks/CTypeDataHandlerHook.php
+++ Classes/Hooks/CTypeDataHandlerHook.php
@@ -32,18 +32,8 @@ class CTypeDataHandlerHook
             }
 
             $colPos = (int)$incomingFieldArray['colPos'];
-            $columnConfiguration = $backendLayoutConfiguration->getConfigurationByColPos($colPos);
-            if (empty($columnConfiguration) || (empty($columnConfiguration['allowed']) && empty($columnConfiguration['disallowed']))) {
-                return;
-            }
-
-            if (!empty($columnConfiguration['allowed'])) {
-                $cTypes = GeneralUtility::trimExplode(',', $columnConfiguration['allowed']);
-                $allowed = in_array($incomingFieldArray['CType'], $cTypes, true);
-            } else {
-                $cTypes = GeneralUtility::trimExplode(',', $columnConfiguration['disallowed']);
-                $allowed = !in_array($incomingFieldArray['CType'], $cTypes, true);
-            }
+            $cType = $incomingFieldArray['CType'];
+            $allowed = $backendLayoutConfiguration->isAllowedCTypeInColPos($cType, $colPos);
 
             if (!$allowed) {
                 unset($dataHandler->datamap['tt_content'][$id]);
@@ -57,7 +47,7 @@ class CTypeDataHandlerHook
                     23,
                     [
                         $incomingFieldArray[$GLOBALS['TCA']['tt_content']['ctrl']['label']],
-                        $incomingFieldArray['CType'],
+                        $cType,
                     ]
                 );
             }
@@ -75,10 +65,62 @@ class CTypeDataHandlerHook
             return;
         }
 
-        $datamap = $dataHandler->datamap;
         foreach ($cmdmap['tt_content'] as $id => $incomingFieldArray) {
-            if (!isset($datamap['tt_content'][$id])) {
-                unset($dataHandler->cmdmap['tt_content'][$id]);
+            foreach ($incomingFieldArray as $command => $value) {
+                switch ($command) {
+                    case 'move':
+                        // New colPos is passed as datamap array and already processed in processDatamap_beforeStart
+                        if (!isset($dataHandler->datamap['tt_content'][$id])) {
+                            unset($dataHandler->cmdmap['tt_content'][$id]);
+                        }
+                        break;
+                    case 'copy':
+                        $currentRecord = BackendUtility::getRecord('tt_content', $id);
+                        $cType = $currentRecord['CType'];
+                        if (is_array($value)
+                            && !empty($value['action'])
+                            && 'paste' === $value['action']
+                            && isset($value['update']['colPos'])
+                        ) {
+                            $pageId = (int)$value['target'];
+                            $backendLayoutConfiguration = BackendLayoutConfiguration::createFromPageId($pageId);
+                            if (null === $backendLayoutConfiguration) {
+                                return;
+                            }
+
+                            $colPos = (int)$value['update']['colPos'];
+                            $allowed = $backendLayoutConfiguration->isAllowedCTypeInColPos($cType, $colPos);
+                        } else {
+                            $targetRecord = BackendUtility::getRecord('tt_content', abs($value));
+                            $pageId = (int)$targetRecord['pid'];
+                            $backendLayoutConfiguration = BackendLayoutConfiguration::createFromPageId($pageId);
+                            if (null === $backendLayoutConfiguration) {
+                                return;
+                            }
+
+                            $colPos = (int)$targetRecord['colPos'];
+                            $allowed = $backendLayoutConfiguration->isAllowedCTypeInColPos($cType, $colPos);
+                        }
+
+                        if (!$allowed) {
+                            unset($dataHandler->cmdmap['tt_content'][$id]);
+                            $dataHandler->log(
+                                'tt_content',
+                                $id,
+                                1,
+                                $pageId,
+                                1,
+                                'The command "%s" for record "%s" with CType "%s" couldn\'t be executed due to disallowed CType value.',
+                                23,
+                                [
+                                    $command,
+                                    $currentRecord[$GLOBALS['TCA']['tt_content']['ctrl']['label']],
+                                    $cType,
+                                ]
+                            );
+                        }
+                        break;
+                }
             }
         }
 
