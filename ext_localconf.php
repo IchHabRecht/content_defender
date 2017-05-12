347fd282e4c5e53e68f341b3fc826f7565043147
Nicole Cordes <typo3@cordes.co>
2017-05-12 17:56:33 +0200

    [FEATURE] Remove loaded columns from edit form
    ColPos items where backend_layout maxitems configuration is reached, are
    removed from the select list within the edit form of a content element.

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,6 +10,12 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRe
     ],
 ];
 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\IchHabRecht\ContentDefender\Form\FormDataProvider\TcaColPosItems::class] = [
+    'depends' => [
+        \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectItems::class,
+    ],
+];
+
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['content_defender'] =
     \IchHabRecht\ContentDefender\Hooks\DatamapDataHandlerHook::class;
 
