826761bc22e49384d1d4a3b88f05cceded9f3fee
Nicole Cordes <typo3@cordes.co>
2017-08-07 13:01:34 +0200

    [RELEASE] Release of content_defender 2.2.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "content_defender".
  *
- * Auto generated 15-06-2017 13:01
+ * Auto generated 07-08-2017 12:59
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '2.2.2',
+  'version' => '2.2.3',
   'constraints' => 
   array (
     'depends' => 
@@ -42,6 +42,6 @@ $EM_CONF[$_EXTKEY] = array (
       'IchHabRecht\\ContentDefender\\Tests\\' => 'Tests/',
     ),
   ),
-  '_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"3bc8";s:9:"README.md";s:4:"4ff8";s:13:"composer.json";s:4:"d108";s:12:"ext_icon.png";s:4:"f488";s:17:"ext_localconf.php";s:4:"195f";s:52:"Classes/BackendLayout/BackendLayoutConfiguration.php";s:4:"8f99";s:54:"Classes/Form/Exception/AccessDeniedColPosException.php";s:4:"3fa2";s:47:"Classes/Form/FormDataProvider/TcaCTypeItems.php";s:4:"cbc4";s:48:"Classes/Form/FormDataProvider/TcaColPosItems.php";s:4:"5527";s:41:"Classes/Hooks/AbstractDataHandlerHook.php";s:4:"5534";s:39:"Classes/Hooks/CmdmapDataHandlerHook.php";s:4:"51e8";s:40:"Classes/Hooks/DatamapDataHandlerHook.php";s:4:"df24";s:33:"Classes/Hooks/WizardItemsHook.php";s:4:"450f";s:47:"Tests/Functional/AbstractFunctionalTestCase.php";s:4:"b8c1";s:44:"Tests/Functional/Fixtures/Database/pages.xml";s:4:"43c6";s:49:"Tests/Functional/Fixtures/Database/tt_content.xml";s:4:"8555";s:61:"Tests/Functional/Fixtures/TSconfig/NewContentElementWizard.ts";s:4:"ca7e";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Default.ts";s:4:"f2cd";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Subpage.ts";s:4:"87bc";s:60:"Tests/Functional/Form/FormDataProvider/TcaCTypeItemsTest.php";s:4:"9fec";s:60:"Tests/Functional/Form/FormDataProvider/TcaColPostemsTest.php";s:4:"0f53";s:52:"Tests/Functional/Hooks/CmpmapDataHandlerHookTest.php";s:4:"6766";s:53:"Tests/Functional/Hooks/DatamapDataHandlerHookTest.php";s:4:"d9cc";s:46:"Tests/Functional/Hooks/WizardItemsHookTest.php";s:4:"7ba7";}',
+  '_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"9df7";s:9:"README.md";s:4:"4ff8";s:13:"composer.json";s:4:"d108";s:12:"ext_icon.png";s:4:"f488";s:17:"ext_localconf.php";s:4:"195f";s:52:"Classes/BackendLayout/BackendLayoutConfiguration.php";s:4:"8f99";s:54:"Classes/Form/Exception/AccessDeniedColPosException.php";s:4:"3fa2";s:47:"Classes/Form/FormDataProvider/TcaCTypeItems.php";s:4:"cbc4";s:48:"Classes/Form/FormDataProvider/TcaColPosItems.php";s:4:"5527";s:41:"Classes/Hooks/AbstractDataHandlerHook.php";s:4:"5534";s:39:"Classes/Hooks/CmdmapDataHandlerHook.php";s:4:"b9d1";s:40:"Classes/Hooks/DatamapDataHandlerHook.php";s:4:"df24";s:33:"Classes/Hooks/WizardItemsHook.php";s:4:"450f";s:47:"Tests/Functional/AbstractFunctionalTestCase.php";s:4:"9fe5";s:44:"Tests/Functional/Fixtures/Database/pages.xml";s:4:"7199";s:49:"Tests/Functional/Fixtures/Database/tt_content.xml";s:4:"2882";s:61:"Tests/Functional/Fixtures/TSconfig/NewContentElementWizard.ts";s:4:"ca7e";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Default.ts";s:4:"f2cd";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Subpage.ts";s:4:"87bc";s:60:"Tests/Functional/Form/FormDataProvider/TcaCTypeItemsTest.php";s:4:"9fec";s:60:"Tests/Functional/Form/FormDataProvider/TcaColPostemsTest.php";s:4:"0f53";s:52:"Tests/Functional/Hooks/CmpmapDataHandlerHookTest.php";s:4:"8272";s:53:"Tests/Functional/Hooks/DatamapDataHandlerHookTest.php";s:4:"d9cc";s:46:"Tests/Functional/Hooks/WizardItemsHookTest.php";s:4:"7ba7";}',
 );
 
