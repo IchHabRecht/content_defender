6d35f3f04c8214006b7d76c03a3c864a48d2b191
Nicole Cordes <typo3@cordes.co>
2017-12-15 23:14:17 +0100

    [TASK] Add TYPO3 9.0 to extension constraints and Travis CI

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-8.7.99',
+      'typo3' => '7.6.0-9.0.99',
     ),
     'conflicts' => 
     array (
