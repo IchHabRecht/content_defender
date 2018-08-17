619fd45e5a144501fa090befbba0c94c1b97b1c6
Nicole Cordes <typo3@cordes.co>
2018-08-17 12:14:09 +0200

    [RELEASE] Release of content_defender 3.0.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "content_defender".
  *
- * Auto generated 16-08-2018 18:25
+ * Auto generated 17-08-2018 12:13
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '3.0.6',
+  'version' => '3.0.7',
   'constraints' => 
   array (
     'depends' => 
@@ -42,6 +42,6 @@ $EM_CONF[$_EXTKEY] = array (
       'IchHabRecht\\ContentDefender\\Tests\\' => 'Tests/',
     ),
   ),
-  '_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"4adc";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"5be2";s:13:"composer.json";s:4:"cb66";s:12:"ext_icon.png";s:4:"f488";s:17:"ext_localconf.php";s:4:"b918";s:16:"phpunit.xml.dist";s:4:"7d0d";s:24:"sonar-project.properties";s:4:"ee02";s:52:"Classes/BackendLayout/BackendLayoutConfiguration.php";s:4:"a9c0";s:54:"Classes/Form/Exception/AccessDeniedColPosException.php";s:4:"7671";s:47:"Classes/Form/FormDataProvider/TcaCTypeItems.php";s:4:"cc53";s:48:"Classes/Form/FormDataProvider/TcaColPosItems.php";s:4:"5b26";s:41:"Classes/Hooks/AbstractDataHandlerHook.php";s:4:"5ceb";s:39:"Classes/Hooks/CmdmapDataHandlerHook.php";s:4:"0160";s:40:"Classes/Hooks/DatamapDataHandlerHook.php";s:4:"4f4b";s:33:"Classes/Hooks/WizardItemsHook.php";s:4:"eea9";s:40:"Classes/Repository/ContentRepository.php";s:4:"fba1";s:47:"Tests/Functional/AbstractFunctionalTestCase.php";s:4:"1d51";s:44:"Tests/Functional/Fixtures/Database/pages.xml";s:4:"7199";s:61:"Tests/Functional/Fixtures/Database/pages_language_overlay.xml";s:4:"49d9";s:49:"Tests/Functional/Fixtures/Database/tt_content.xml";s:4:"8c39";s:61:"Tests/Functional/Fixtures/TSconfig/NewContentElementWizard.ts";s:4:"ca7e";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Default.ts";s:4:"a875";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Subpage.ts";s:4:"87bc";s:60:"Tests/Functional/Form/FormDataProvider/TcaCTypeItemsTest.php";s:4:"506f";s:60:"Tests/Functional/Form/FormDataProvider/TcaColPostemsTest.php";s:4:"2c80";s:52:"Tests/Functional/Hooks/CmdmapDataHandlerHookTest.php";s:4:"a2e3";s:53:"Tests/Functional/Hooks/DatamapDataHandlerHookTest.php";s:4:"0a8a";s:46:"Tests/Functional/Hooks/WizardItemsHookTest.php";s:4:"24ea";s:47:"Tests/Unit/Repository/ContentRepositoryTest.php";s:4:"d853";}',
+  '_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"526c";s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"5be2";s:13:"composer.json";s:4:"cb66";s:12:"ext_icon.png";s:4:"f488";s:17:"ext_localconf.php";s:4:"b918";s:16:"phpunit.xml.dist";s:4:"7d0d";s:24:"sonar-project.properties";s:4:"ee02";s:52:"Classes/BackendLayout/BackendLayoutConfiguration.php";s:4:"a9c0";s:54:"Classes/Form/Exception/AccessDeniedColPosException.php";s:4:"7671";s:47:"Classes/Form/FormDataProvider/TcaCTypeItems.php";s:4:"cc53";s:48:"Classes/Form/FormDataProvider/TcaColPosItems.php";s:4:"5b26";s:41:"Classes/Hooks/AbstractDataHandlerHook.php";s:4:"5ceb";s:39:"Classes/Hooks/CmdmapDataHandlerHook.php";s:4:"0160";s:40:"Classes/Hooks/DatamapDataHandlerHook.php";s:4:"4f4b";s:33:"Classes/Hooks/WizardItemsHook.php";s:4:"eea9";s:40:"Classes/Repository/ContentRepository.php";s:4:"fba1";s:47:"Tests/Functional/AbstractFunctionalTestCase.php";s:4:"1d51";s:44:"Tests/Functional/Fixtures/Database/pages.xml";s:4:"7199";s:61:"Tests/Functional/Fixtures/Database/pages_language_overlay.xml";s:4:"49d9";s:49:"Tests/Functional/Fixtures/Database/tt_content.xml";s:4:"8c39";s:61:"Tests/Functional/Fixtures/TSconfig/NewContentElementWizard.ts";s:4:"ca7e";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Default.ts";s:4:"a875";s:60:"Tests/Functional/Fixtures/TSconfig/BackendLayouts/Subpage.ts";s:4:"87bc";s:60:"Tests/Functional/Form/FormDataProvider/TcaCTypeItemsTest.php";s:4:"506f";s:60:"Tests/Functional/Form/FormDataProvider/TcaColPostemsTest.php";s:4:"2c80";s:52:"Tests/Functional/Hooks/CmdmapDataHandlerHookTest.php";s:4:"a2e3";s:53:"Tests/Functional/Hooks/DatamapDataHandlerHookTest.php";s:4:"0a8a";s:46:"Tests/Functional/Hooks/WizardItemsHookTest.php";s:4:"24ea";s:47:"Tests/Unit/Repository/ContentRepositoryTest.php";s:4:"d853";}',
 );
 
