d545bdcd82f6e9213bace917c3e0fbedf5c054c4
Nicole Cordes <typo3@cordes.co>
2019-11-29 14:02:09 +0100

    [BUGFIX] Configure correct colspan in default fixture

--- Tests/Functional/Fixtures/TSconfig/BackendLayouts/Default.ts
+++ Tests/Functional/Fixtures/TSconfig/BackendLayouts/Default.ts
@@ -1,7 +1,7 @@
 mod.web_layout.BackendLayouts.default {
     title = Default
     config.backend_layout {
-        colCount = 3
+        colCount = 6
         rowCount = 3
         rows {
             1 {
@@ -15,6 +15,7 @@ mod.web_layout.BackendLayouts.default {
                     2 {
                         name = Disabled
                         colPos =
+                        colspan = 1
                     }
                 }
             }
@@ -40,6 +41,7 @@ mod.web_layout.BackendLayouts.default {
                     1 {
                         name = Footer1 (no bullets)
                         colPos = 10
+                        colspan = 2
                         disallowed {
                             CType = bullets
                         }
@@ -48,6 +50,7 @@ mod.web_layout.BackendLayouts.default {
                     2 {
                         name = Footer2 (bullets)
                         colPos = 11
+                        colspan = 2
                         allowed {
                             CType = bullets
                         }
@@ -56,6 +59,7 @@ mod.web_layout.BackendLayouts.default {
                     3 {
                         name = Footer3 (all, tx_simpleselectboxsingle=5)
                         colPos = 12
+                        colspan = 2
                         allowed {
                             tx_simpleselectboxsingle = 5
                         }
