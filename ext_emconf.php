460cf9a080c6d6f53e20b09446fe9b7946738bc3
Nicole Cordes <typo3@cordes.co>
2020-04-22 13:48:11 +0200

    [TASK] Raise compatibility to TYPO3 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '8.7.0-10.2.99',
+      'typo3' => '8.7.0-10.4.99',
     ),
     'conflicts' =>
     array (
