50b072c8477d779aac00ebc10d6b52d8edfd9099
Nicole Cordes <typo3@cordes.co>
2024-04-29 09:35:37 +0200

    [RELEASE] Release of content_defender 3.4.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '3.4.2',
+  'version' => '3.4.3',
   'constraints' =>
   array (
     'depends' =>
